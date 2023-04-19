8e15f08e968e862dc2ce3704c8c5290855fc3ad2
Rian van der Merwe <rianvdm@gmail.com>
2023-04-19 13:21:40 -0700

    Top albums

---
+++ netlify/functions/getTopAlbums/getTopAlbums.js
@@ -0,0 +1,35 @@
+// Get top albums from last.fm
+
+const lastFMToken = process.env.LAST_FM_API_TOKEN;
+const lastFMUser = 'bordesak';
+
+const fetch = require('node-fetch');
+
+const handler = async () => {
+  const url = `http://ws.audioscrobbler.com/2.0/?method=user.gettopalbums&user=${lastFMUser}&api_key=${lastFMToken}&period=7day&format=json`
+  const results = await fetch(url);
+
+  // If there was an error
+  if (!results.ok) {
+    console.error(results);
+
+    return {
+      statusCode: 500,
+      body: results.statusText
+    }
+  }
+
+  // Get JSON body from results
+  const data = await results.json();
+
+  // Return data
+  return {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  }
+}
+
+module.exports = { handler }
---
+++ public/lastfm-top.js
@@ -0,0 +1,24 @@
+fetch('/.netlify/functions/getTopAlbums')
+  .then(response => response.json())
+  .then(data => {
+    const dataContainer = document.querySelector('.js-lastfm-top');
+    const topAlbums = data.topalbums.album.slice(0, 6);
+
+      const html = topAlbums.map(album => `
+        <div class="track">
+            <a href="${album.url}" target="_blank" class="track_link">
+            <img src="${album.image[3]['#text']}" class="track_image">
+            <div class="track_content">
+              <h2 class="track_artist">${album.name}</h2>
+              <p class="track_name">${album.artist.name}</p>
+            </div>
+          </a>
+        </div>
+      `).join('');
+      dataContainer.innerHTML = html;
+  
+  })
+  .catch(error => console.error(error));
+
+
+
