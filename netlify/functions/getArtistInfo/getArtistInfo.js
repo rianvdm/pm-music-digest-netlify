0ac5cc81e136fa3182d8e89f524b414b04ffaf15
Rian van der Merwe <rianvdm@gmail.com>
2023-04-20 10:33:59 -0700

    Add artist info

---
+++ netlify/functions/getArtistInfo/getArtistInfo.js
@@ -0,0 +1,35 @@
+const lastFMToken = process.env.LAST_FM_API_TOKEN;
+const fetch = require('node-fetch');
+
+const handler = async (event) => {
+  // Get mbid from query parameters
+  const mbid = event.queryStringParameters.mbid;
+
+  // Make request to Last.fm API
+  const url = `http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&mbid=${mbid}&api_key=${lastFMToken}&format=json`;
+  const results = await fetch(url);
+
+  // If there was an error
+  if (!results.ok) {
+    console.error(results);
+
+    return {
+      statusCode: 500,
+      body: results.statusText
+    }
+  }
+
+  // Get JSON body from results
+  const data = await results.json();
+
+  // Return data
+  return {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  }
+}
+
+module.exports = { handler }
--- public/lastfm-top-artists.js
+++ public/lastfm-top-artists.js
@@ -4,12 +4,59 @@ fetch('/.netlify/functions/getTopArtists')
     const dataContainer = document.querySelector('.js-lastfm-top-artists');
     const topArtists = data.topartists.artist.slice(0, 5);
 
-      const html = topArtists.map(artist => `
-        <div class="track_ul">
-            ${artist['@attr'].rank}. <a href="${artist.url}" target="_blank" class="track_link">${artist.name}</a> with ${artist.playcount} songs played.
-        </div>
-      `).join('');
-      dataContainer.innerHTML = html;
-  
+    // Create an array of promises for each artist's summary and first tag
+    const artistPromises = topArtists.map(artist => {
+      return fetch(`/.netlify/functions/getArtistInfo?mbid=${artist.mbid}`)
+        .then(response => response.json())
+        .then(data => {
+          // Check for error property in Last.fm API response
+          if (data.error) {
+            return {
+              summary: 'Last.fm unfortunately does not have additional information on this artist.',
+            };
+          }
+
+          // Return the summary and first tag if they exist
+          return {
+            tag1: data.artist.tags.tag.length > 0 ? data.artist.tags.tag[0].name : '',
+            tag2: data.artist.tags.tag.length > 0 ? data.artist.tags.tag[1].name : '',
+            similarArtist1: data.artist.similar.artist.length > 0 ? data.artist.similar.artist[0].name : '',
+            similarArtist2: data.artist.similar.artist.length > 0 ? data.artist.similar.artist[1].name : '',
+            similarArtist1URL: data.artist.similar.artist.length > 0 ? data.artist.similar.artist[0].url : '',
+            similarArtist2URL: data.artist.similar.artist.length > 0 ? data.artist.similar.artist[1].url : '',
+          };
+        })
+        .catch(error => {
+          console.error(error);
+          return null;
+        });
+    });
+
+    // Resolve all artist promises and create HTML
+    Promise.all(artistPromises)
+      .then(artists => {
+        const html = topArtists.map((artist, i) => {
+          if (artists[i].summary) {
+            return `
+              <div class="track_ul">
+                  ${artist['@attr'].rank}. <strong><a href="${artist.url}" target="_blank" class="track_link">${artist.name}</a></strong> (${artist.playcount} plays). 
+                  ${artists[i].summary}
+              </div>
+            `;
+          } else {
+            return `
+              <div class="track_ul">
+                  ${artist['@attr'].rank}. <strong><a href="${artist.url}" target="_blank" class="track_link">${artist.name}</a></strong> (${artist.playcount} plays). 
+                  Their primary genres are <strong>${artists[i].tag1}</strong> and <strong>${artists[i].tag2}</strong>, 
+                  and you might like them if you are into <a href="${artists[i].similarArtist1URL}" target="_blank" class="track_link">${artists[i].similarArtist1}</a> or <a href="${artists[i].similarArtist2URL}" target="_blank" class="track_link">${artists[i].similarArtist2}</a>.
+              </div>
+            `;
+          }
+        }).join('');
+        dataContainer.innerHTML = html;
+      })
+      .catch(error => console.error(error));
+
+
   })
   .catch(error => console.error(error));
