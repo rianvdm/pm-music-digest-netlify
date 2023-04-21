77864b978dc48a1a281ed0fad060ab90739957c4
Rian van der Merwe <rianvdm@gmail.com>
2023-04-21 13:29:14 -0700

    More data added in a bunch of places.

--- public/discogs-added.js
+++ public/discogs-added.js
@@ -5,7 +5,7 @@ fetch('/.netlify/functions/getDiscogsCollection')
   .then(response => response.json())
   .then(data => {
     const dataContainer = document.querySelector('.js-discogs-added');
-    const discogsAdded = data.releases.slice(0, 8);
+    const discogsAdded = data.releases.slice(0, 6);
 
       const html = discogsAdded.map(releases => {
       const dateAdded = releases.date_added;
@@ -19,7 +19,10 @@ fetch('/.netlify/functions/getDiscogsCollection')
             <div class="track_content">
               <h2 class="track_artist">${releases.basic_information.title}</h2>
               <p class="track_name">${releases.basic_information.artists[0].name}</p></a>
-              <p class="track_album">${releases.basic_information.formats[0].name} added on ${formattedDate}</p>
+              <p class="track_album">${releases.basic_information.formats[0].name} added on ${formattedDate}.</p>
+              <p class="track_album">It is a ${releases.basic_information.genres[0]} album 
+              on the ${releases.basic_information.labels[0].name} label and it
+              ${releases.basic_information.year != 0 ? `was released in ${releases.basic_information.year}.` : `has an unknown release date.`}</p>
             </div>
         </div>
       `;
@@ -28,4 +31,3 @@ fetch('/.netlify/functions/getDiscogsCollection')
   
   })
   .catch(error => console.error(error));
-
