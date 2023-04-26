9c429c1e1447993cd781358887f6bfda779b7bc8
Rian van der Merwe <rianvdm@gmail.com>
2023-04-26 13:55:10 -0700

    Spotify additions

--- public/lastfm-loved-tracks.js
+++ public/lastfm-loved-tracks.js
@@ -2,7 +2,7 @@ fetch('/.netlify/functions/getLovedTracks')
   .then(response => response.json())
   .then(async data => {
     const dataContainer = document.querySelector('.js-lastfm-loved-tracks');
-    const lovedTracks = data.lovedtracks.track.slice(0, 6);
+    const lovedTracks = data.lovedtracks.track.slice(0, 5);
 
     // Create an array of promises for each artist's data
     const trackPromises = lovedTracks.map(track => {
@@ -15,6 +15,7 @@ fetch('/.netlify/functions/getLovedTracks')
               summary: 'Last.fm unfortunately does not have any additional information on this artist.',
             };
           }
+
 
           // Return the data if it exists
           return {
@@ -30,39 +31,47 @@ fetch('/.netlify/functions/getLovedTracks')
         });
     });
 
-    // Resolve all artist promises and create HTML
-    Promise.all(trackPromises)
-      .then(tracks => {
+// Resolve all artist promises and create HTML
+Promise.all(trackPromises)
+  .then(async tracks => {
+    const html = await Promise.all(lovedTracks.map(async (track, i) => {
 
-        const html = lovedTracks.map((track, i) => {
-          if (tracks[i].summary) {
-            return `
-              <li class="track_ul">
-                  ${tracks[i].summary}
-              </li>
-            `;
-          } else {
-            const q = `${track.name} ${track.artist.name}`
-            const optionsDate = { year: 'numeric', month: 'long', day: 'numeric' };
-            const pacificTimezone = 'America/Los_Angeles';
-            const utsDate = track.date.uts;
-            const formattedDate = new Date(utsDate * 1000).toLocaleString('en-US', {
-              ...optionsDate,
-              timeZone: pacificTimezone
-              });
-            return `
-              <li class="track_ul">
-                <strong>${track.name}</strong></a> by <strong>${track.artist.name}</strong> (recommended on ${formattedDate}).
-                <br><a href="https://songwhip.com/create?q=${q}" target="_blank">Stream now</a> if you like ${tracks[i].tags[0].name} / ${tracks[i].tags[1].name} music 
-                from artists like ${tracks[i].similarArtist[0].name}, ${tracks[i].similarArtist[1].name}, and ${tracks[i].similarArtist[2].name}.
-              </li>
-            `;
-          }
-        }).join('');
-        dataContainer.innerHTML = `<ol>${html}</ol>`;
-      })
-      .catch(error => console.error(error));
+      const q = `${track.name} ${track.artist.name}`;
+      const spotifyResponse = await fetch(`/.netlify/functions/getSpotifySong?q=${encodeURIComponent(q)}`);
+      const spotifyData = await spotifyResponse.json();
+      const spotifyUrl = spotifyData.tracks.items[0].external_urls.spotify;
+      const spotifyID = spotifyData.tracks.items[0].id;
+
+      if (tracks[i].summary) {
+        return `
+          <li class="track_ul">
+            ${tracks[i].summary}
+          </li>
+        `;
+      } else {
+        const optionsDate = { year: 'numeric', month: 'long', day: 'numeric' };
+        const pacificTimezone = 'America/Los_Angeles';
+        const utsDate = track.date.uts;
+        const formattedDate = new Date(utsDate * 1000).toLocaleString('en-US', {
+          ...optionsDate,
+          timeZone: pacificTimezone
+        });
+        return `
+          <li class="track_ul">
+            <strong>${track.name}</strong> by <strong>${track.artist.name}</strong> (recommended on ${formattedDate}).
+            <br><a href="https://songwhip.com/${spotifyUrl}" target="_blank">Stream now</a> if you like ${tracks[i].tags[0].name} / ${tracks[i].tags[1].name} music from artists like ${tracks[i].similarArtist[0].name}, ${tracks[i].similarArtist[1].name}, and ${tracks[i].similarArtist[2].name}.
+          </li>
+        `;
+      }
+    }));
+    dataContainer.innerHTML = `<ol>${html.join('')}</ol>`;
+  })
+  .catch(error => console.error(error));
+
 
 
   })
   .catch(error => console.error(error));
+
+// <br><br><iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/${spotifyID}"
+// width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe><br><br>
