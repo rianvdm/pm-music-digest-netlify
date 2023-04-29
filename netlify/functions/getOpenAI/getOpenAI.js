c0732205e33ba30accb4285cd6dc120d83831ed2
Rian van der Merwe <rianvdm@gmail.com>
2023-04-29 10:48:33 -0700

    Update getOpenAI.js

--- netlify/functions/getOpenAI/getOpenAI.js
+++ netlify/functions/getOpenAI/getOpenAI.js
@@ -22,7 +22,7 @@ exports.handler = async function(event, context) {
         prompt: prompt,
         max_tokens: 100,
         n: 1,
-        temperature: 0.8,
+        temperature: 1,
       }),
     });
 
