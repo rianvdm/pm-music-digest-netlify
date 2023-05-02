50009072b0f3d7da10e76cc2ddbc9605918d188f
Rian van der Merwe <rianvdm@gmail.com>
2023-05-02 10:40:56 -0700

    Rollbacks

--- public/lastfm-loved-tracks.js
+++ public/lastfm-loved-tracks.js
@@ -1,42 +1,54 @@
 const encodeName = (name) => encodeURIComponent(name.replace(/&/g, '%26').replace(/\+/g, '%2B').replace(/\./g, '%2E'));
 
-async function fetchData(url, extractData = false) {
-  try {
-    const response = await fetch(url);
-    const jsonResponse = await response.json();
-    return extractData ? jsonResponse.data : jsonResponse;
-  } catch (error) {
-    console.error(error);
-  }
-}
-
-async function fetchAndDisplayLovedTracks() {
-  try {
-    const data = await fetchData('/.netlify/functions/getLovedTracks');
+fetch('/.netlify/functions/getLovedTracks')
+  .then(response => response.json())
+  .then(async data => {
     const dataContainer = document.querySelector('.js-lastfm-loved-tracks');
     const lovedTracks = data.lovedtracks.track.slice(0, 5);
 
     const trackPromises = lovedTracks.map(async track => {
       const encodedArtist = encodeName(track.artist.name);
       const encodedTrack = encodeName(track.name);
+
+      const lastfmPromise = fetch(`/.netlify/functions/getLastfmData?type=getArtistInfo&artist=${encodedArtist}`)
+        .then(response => response.json())
+        .catch(error => {
+          console.error(error);
+          return null;
+        });
+
       const q = `${encodedTrack} ${encodedArtist}`;
+      const spotifySearchPromise = fetch(`/.netlify/functions/getSpotifySearchResults?type=getTrack&q=${q}`)
+        .then(response => response.json());
 
-      const spotifyData = await fetchData(`/.netlify/functions/getSpotifySearchResults?type=getTrack&q=${q}`, true);
-      const spotifyUrl = spotifyData.items[0].external_urls.spotify;
-      const spotifyID = spotifyData.items[0].id;
-      const spotifyArtistID = spotifyData.items[0].artists[0].id;
-      const spotifyImgUrl = spotifyData.items[0].album.images[1].url;
-      const spotifyGenres = "indie, jazz, rock";
+      const [lastfmData, spotifyData] = await Promise.allSettled([lastfmPromise, spotifySearchPromise]);
 
-      const spotifyRecoData = await fetchData(`/.netlify/functions/getSpotifyRecommendations?seed_artists=${spotifyArtistID}&seed_genres=${spotifyGenres}&seed_tracks=${spotifyID}`);
+      const lastfmTags = lastfmData.value && lastfmData.value.artist.tags.tag
+        .filter(tag => tag.name.toLowerCase() !== "seen live")
+        .slice(0, 2);
+
+      const spotifyUrl = spotifyData.value.data.items[0].external_urls.spotify;
+      const spotifyID = spotifyData.value.data.items[0].id;
+      const spotifyArtistID = spotifyData.value.data.items[0].artists[0].id;
+      const spotifyImgUrl = spotifyData.value.data.items[0].album.images[1].url;
+      const spotifyGenres = (lastfmTags && (lastfmTags[0]?.name || lastfmTags[1]?.name))
+        ? `${lastfmTags[0]?.name || ""}, ${lastfmTags[1]?.name || ""}`
+        : "rock, indie, jazz";
+
+      const spotifyRecoPromise = fetch(`/.netlify/functions/getSpotifyRecommendations?seed_artists=${spotifyArtistID}&seed_genres=${spotifyGenres}&seed_tracks=${spotifyID}`)
+        .then(response => response.json());
 
       const prompt = `Write a summary to help someone decide if they might like the song ${encodeURIComponent(track.name)} by ${encodeURIComponent(track.artist.name)}. Include information about the song/artist’s genres as well as similar artists. Write no more than one sentence.`;
       const max_tokens = 80;
 
-      const openaiData = await fetchData(`/.netlify/functions/getOpenAI?prompt=${prompt}&max_tokens=${max_tokens}`, true);
+      const openaiPromise = fetch(`/.netlify/functions/getOpenAI?prompt=${prompt}&max_tokens=${max_tokens}`)
+        .then(response => response.json());
+
+      const [spotifyRecoData, openaiData] = await Promise.allSettled([spotifyRecoPromise, openaiPromise]);
 
       return {
         track,
+        lastfmTags,
         spotifyUrl,
         spotifyID,
         spotifyImgUrl,
@@ -47,12 +59,12 @@ async function fetchAndDisplayLovedTracks() {
 
     const trackData = await Promise.all(trackPromises);
 
-    const html = trackData.map(({ track, spotifyUrl, spotifyID, spotifyImgUrl, spotifyRecoData, openaiData }) => {
-      const spotifyTrackReco = spotifyRecoData.tracks.slice(0, 2).map(track => track.name);
-      const spotifyArtistReco = spotifyRecoData.tracks.slice(0, 2).map(track => track.artists[0].name);
-      const spotifyUrlsReco = spotifyRecoData.tracks.slice(0, 2).map(track => track.external_urls.spotify);
+    const html = trackData.map(({ track, lastfmTags, spotifyUrl, spotifyID, spotifyImgUrl, spotifyRecoData, openaiData }) => {
+      const spotifyTrackReco = spotifyRecoData.value.tracks.slice(0, 3).map(track => track.name);
+      const spotifyArtistReco = spotifyRecoData.value.tracks.slice(0, 3).map(track => track.artists[0].name);
+      const spotifyUrlsReco = spotifyRecoData.value.tracks.slice(0, 3).map(track => track.external_urls.spotify);
 
-      const openaiTextResponse = openaiData.choices[0].message['content'];
+      const openaiTextResponse = openaiData.value.data.choices[0].message['content'];
 
       const optionsDate = { year: 'numeric', month: 'long', day: 'numeric' };
       const pacificTimezone = 'America/Los_Angeles';
@@ -76,11 +88,5 @@ async function fetchAndDisplayLovedTracks() {
     });
 
     dataContainer.innerHTML = `${html.join('')}`;
-  } catch (error) {
-    console.error(error);
-  }
-}
-
-fetchAndDisplayLovedTracks();
-
-
+  })
+  .catch(error => console.error(error));
