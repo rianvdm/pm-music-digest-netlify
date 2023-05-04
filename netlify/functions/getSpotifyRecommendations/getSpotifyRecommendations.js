6848134c5ae34426bbb177d8cff52f0b790a55b2
Rian van der Merwe <rianvdm@gmail.com>
2023-05-04 11:52:00 -0700

    Ok, let's bring Redis back

--- netlify/functions/getSpotifyRecommendations/getSpotifyRecommendations.js
+++ netlify/functions/getSpotifyRecommendations/getSpotifyRecommendations.js
@@ -1,22 +1,53 @@
 const fetch = require("node-fetch");
+const Redis = require("ioredis");
 
-exports.handler = async function(event, context) {
+const client = new Redis(process.env.REDIS_URL);
+
+exports.handler = async function (event, context) {
   try {
     const seed_artists = event.queryStringParameters.seed_artists;
     const seed_genres = event.queryStringParameters.seed_genres;
     const seed_tracks = event.queryStringParameters.seed_tracks;
 
-    // Get the URL of the getSpotifyToken function from an environment variable
     const getTokenUrl = process.env.GET_SPOTIFY_TOKEN_URL;
 
-    // Call the getSpotifyToken function to retrieve an access token
-    const tokenResponse = await fetch(getTokenUrl);
-    const { access_token } = await tokenResponse.json();
+    console.log('Creating Redis client');
+    const client = new Redis(process.env.REDIS_URL, {
+      connectTimeout: 10000,
+    });
 
-    // Construct the request URL with the necessary query parameters
+    client.on('error', (error) => {
+      console.error('Error in Redis client:', error);
+    });
+
+    console.log('Retrieving access token and expiration time from Redis');
+    let access_token = await client.get("spotify_access_token");
+    let expires_at_str = await client.get("spotify_expires_at");
+    let expires_at = parseInt(expires_at_str, 10);
+    console.log(expires_at);
+
+    if (!access_token || !expires_at || Date.now() >= expires_at) {
+      console.log('Fetching new access token');
+      const tokenResponse = await fetch(getTokenUrl);
+      const tokenData = await tokenResponse.json();
+      console.log(tokenData);
+      access_token = tokenData.access_token;
+      expires_at = Date.now() + tokenData.expires_in * 1000;
+      console.log(expires_at);
+
+      console.log('Storing new access token and expiration time in Redis');
+      await client.set("spotify_access_token", access_token);
+      await client.set("spotify_expires_at", expires_at);
+    } else {
+      console.log('Using existing access token from Redis');
+      console.log(expires_at);
+    }
+
+    console.log('Quitting Redis client');
+    await client.quit();
+
     const requestUrl = `https://api.spotify.com/v1/recommendations?limit=10&seed_artists=${seed_artists}&seed_genres=${seed_genres}&seed_tracks=${seed_tracks}&limit=2`;
 
-    // Make the request to the Spotify API with the access token
     const response = await fetch(requestUrl, {
       method: "GET",
       headers: {
@@ -25,14 +56,12 @@ exports.handler = async function(event, context) {
       },
     });
 
-    // Parse the response as JSON and return it as the response of the Netlify function
     const data = await response.json();
     return {
       statusCode: 200,
       body: JSON.stringify(data),
     };
   } catch (error) {
-    // Handle any errors that occur during the request
     console.error(error);
     return {
       statusCode: 500,
