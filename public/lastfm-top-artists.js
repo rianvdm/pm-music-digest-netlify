29717bda457d935e03256b5c1466ea798df6118d
Rian van der Merwe <rianvdm@gmail.com>
2023-05-07 08:41:51 -0700

    Spotify for top artist songs

--- public/lastfm-top-artists.js
+++ public/lastfm-top-artists.js
@@ -38,7 +38,7 @@ fetch('/.netlify/functions/getTopArtists?period=7day')
               .filter(tag => tag.name !== "seen live")
               .slice(0, 3),
             similarArtist: data.artist.similar.artist.slice(0,3),
-            topAlbums: albumData.topalbums.album.slice(0, 2),
+            // topAlbums: albumData.topalbums.album.slice(0, 2),
             // bio: bio
           };
         })
@@ -61,6 +61,16 @@ fetch('/.netlify/functions/getTopArtists?period=7day')
           const spotifyArtistImgUrl = spotifyData.data.items[0].images[1].url;
           const spotifyGenres = spotifyData.data.items[0].genres.slice(0, 2);
 
+          async function getTopTracks(spotifyArtistID) {
+            const topTracksResponse = await fetch(`/.netlify/functions/getSpotifyArtistTopTracks?spotifyArtistID=${spotifyArtistID}`);
+            const topTracksData = await topTracksResponse.json();
+            return topTracksData.slice(0, 3); // Directly slice the tracks array
+          }
+
+
+          const topTracks = await getTopTracks(spotifyArtistID);
+
+
           if (artists[i].summary) {
             return `
               <div class="track_ul">
@@ -74,19 +84,19 @@ fetch('/.netlify/functions/getTopArtists?period=7day')
                 <img src="${spotifyArtistImgUrl}">
                 <div class="no-wrap-text">
                   <strong><a href="${artist.url}" target="_blank" class="track_link">${artist.name}</a></strong> (${artist.playcount} plays).
-                  <!-- <br>${artists[i].bio}. -->
                   <br><strong>Genres:</strong> ${artists[i].tags[0].name} and ${artists[i].tags[1].name}.
                   <!-- <br><strong>Genres:</strong> ${spotifyGenres[0]} and ${spotifyGenres[1]}. -->
-                  <br><strong>Most popular albums:</strong> ${
-                    artists[i].topAlbums && artists[i].topAlbums.length >= 2
-                      ? `<a href="${artists[i].topAlbums[0].url}" target="_blank">${artists[i].topAlbums[0].name}</a> and <a href="${artists[i].topAlbums[1].url}" target="_blank">${artists[i].topAlbums[1].name}</a>`
-                      : "unknown"
+                  <br><strong>Most popular songs:</strong> ${
+                      topTracks && topTracks.length >= 3
+                        ? `<a href="https://odesli.co/${topTracks[0].external_urls.spotify}">${topTracks[0].name}</a>, <a href="https://odesli.co/${topTracks[1].external_urls.spotify}">${topTracks[1].name}</a>, and <a href="https://odesli.co/${topTracks[2].external_urls.spotify}">${topTracks[2].name}</a>`
+                        : "unknown"
                   }.
                   <br><strong>Similar artists:</strong> ${
                       artists[i].similarArtist && artists[i].similarArtist.length >= 3
                         ? `<a href="${artists[i].similarArtist[0].url}" target="_blank"">${artists[i].similarArtist[0].name}</a>, <a href="${artists[i].similarArtist[1].url}" target="_blank"">${artists[i].similarArtist[1].name}</a>, and <a href="${artists[i].similarArtist[2].url}" target="_blank">${artists[i].similarArtist[2].name}</a>`
                         : "unknown"
                     }.
+
                 </div>
               </div>
             `;
