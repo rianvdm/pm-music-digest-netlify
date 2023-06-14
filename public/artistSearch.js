6054ffaa93feb7fcef1b621ffd4c6ab946f5f918
Rian van der Merwe <rianvdm@gmail.com>
2023-06-14 13:49:41 -0700

    Update artistSearch.js

--- public/artistSearch.js
+++ public/artistSearch.js
@@ -155,7 +155,7 @@ async function performSearch(artistName) {
 
     // Start OpenAI call
       const prompt = `Write a summary to help someone decide if they might like the artist ${artist.name}. Include information about the artist’s history, genres, and styles. Write no more than 3 paragraphs.`;
-      const max_tokens = 250;
+      const max_tokens = 300;
 
       async function getOpenAiSummary(prompt, max_tokens) {
         const OpenAiSummaryData = await fetchData('getOpenAIArtist', {prompt: prompt, max_tokens: max_tokens, name: artist.name});
