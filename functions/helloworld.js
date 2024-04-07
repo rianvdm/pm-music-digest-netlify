00584c9758570d8ec7d1e59958ec330e5a6adde7
Allan Kent <allan.kent@elerian.ai>
2024-04-07 18:07:43 +0200

    testing url params and environment variables

--- functions/helloworld.js
+++ functions/helloworld.js
@@ -1,3 +1,12 @@
-export function onRequest(context) {
-  return new Response("Hello, world!")
+const LAST_FM_API_TOKEN = '<your-lastfm-api-token>';
+const LASTFM_USERNAME = '<your-lastfm-username>';
+
+addEventListener('fetch', event => {
+  event.respondWith(handleRequest(event.request))
+})
+
+async function handleRequest(request) {
+  const { urlParams } = new URL(request.url);
+  let limit = urlParams.get('limit');
+  return new Response(`Hello ${LASTFM_USERNAME}, ${limit} world!`)
 }
