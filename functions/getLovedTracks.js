d3e891920bcafb3499b01fccbfc0fd3866ed156f
Allan Kent <allan.kent@elerian.ai>
2024-04-07 21:10:20 +0200

    last fm loved tracks

---
+++ functions/getLovedTracks.js
@@ -0,0 +1,30 @@
+export async function onRequest(context) {
+  const LASTFM_USERNAME = context.env.LASTFM_USERNAME;
+  const LASTFM_API_TOKEN = context.env.LAST_FM_API_TOKEN;
+  
+  const { searchParams } = new URL(context.request.url);
+  let limit = searchParams.get('limit');
+  if (!limit || isNaN(parseInt(limit))) {
+    limit = 1; 
+  }
+
+  const url = `http://ws.audioscrobbler.com/2.0/?method=user.getlovedtracks&user=${LASTFM_USERNAME}&api_key=${LASTFM_API_TOKEN}&format=json&limit=${limit}`
+
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Failed to fetch data from Last.fm: ${response.statusText}`);
+    }
+
+    const data = await response.json();
+    return new Response(JSON.stringify(data), {
+      status: 200,
+      headers: { 'Content-Type': 'application/json' },
+    });
+  } catch (error) {
+    return new Response(JSON.stringify({ error: error.message }), {
+      status: 500,
+      headers: { 'Content-Type': 'application/json' },
+    });
+  }
+}
