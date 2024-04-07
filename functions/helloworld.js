853284558c16bdedb921076743c470eaaea92879
Allan Kent <allan.kent@elerian.ai>
2024-04-07 20:06:54 +0200

    searchParams is a property, not a variable name, you cannot change it :)

--- functions/helloworld.js
+++ functions/helloworld.js
@@ -2,8 +2,8 @@ export async function onRequest(context) {
   const LASTFM_USERNAME = context.env.LASTFM_USERNAME;
   const LASTFM_API_TOKEN = context.env.LAST_FM_API_TOKEN;
   
-  const urlParams = new URL(context.request.url);
-  let limit = urlParams.get('limit');
+  const { searchParams } = new URL(context.request.url);
+  let limit = searchParams.get('limit');
   if (!limit || isNaN(parseInt(limit))) {
     limit = 1; 
   }
