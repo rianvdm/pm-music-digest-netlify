a3db1ceafa4d5c06f8bf6a39ccdf8737fb4fe969
Allan Kent <allan.kent@elerian.ai>
2024-04-07 18:13:20 +0200

    remove handler function

--- functions/helloworld.js
+++ functions/helloworld.js
@@ -1,12 +1,9 @@
 const LAST_FM_API_TOKEN = '<your-lastfm-api-token>';
 const LASTFM_USERNAME = '<your-lastfm-username>';
 
-addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request))
-})
 
-async function handleRequest(request) {
-  const { urlParams } = new URL(request.url);
+export async function onRequest(context) {
+  const { urlParams } = new URL(context.request.url);
   let limit = urlParams.get('limit');
   return new Response(`Hello ${LASTFM_USERNAME}, ${limit} world!`)
 }
