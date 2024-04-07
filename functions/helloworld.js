be5d3dd83adb658118551643d41793970d290193
Allan Kent <allan.kent@elerian.ai>
2024-04-07 19:49:24 +0200

    Trying to find the environment

--- functions/helloworld.js
+++ functions/helloworld.js
@@ -1,10 +1,10 @@
-const LAST_FM_API_TOKEN = '<your-lastfm-api-token>';
-const LASTFM_USERNAME = '<your-lastfm-username>';
+// const LAST_FM_API_TOKEN = '<your-lastfm-api-token>';
+// const LASTFM_USERNAME = '<your-lastfm-username>';
 
 
 export async function onRequest(context) {
-  console.log(`Context: ${context}`);
-  console.log(`Username: ${LASTFM_USERNAME}`);
+  console.log(`Context: ${context.request}`);
+  console.log(`Username: ${context.env}`);
   return new Response("Hi");
   // const { urlParams } = new URL(context.request.url);
   // let limit = urlParams.get('limit');
