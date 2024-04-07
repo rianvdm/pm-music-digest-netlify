3073e675fb898919b226ba960d44f9c9519feb0f
Allan Kent <allan.kent@elerian.ai>
2024-04-07 20:13:43 +0200

    no logs :(

--- functions/helloworld.js
+++ functions/helloworld.js
@@ -9,22 +9,22 @@ export async function onRequest(context) {
   }
 
   const url = `http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=${LASTFM_USERNAME}&api_key=${LAST_FM_API_TOKEN}&limit=${limit}&format=json`;
-
-  try {
-    const response = await fetch(url);
-    if (!response.ok) {
-      throw new Error(`Failed to fetch data from Last.fm: ${response.statusText}`);
-    }
-
-    const data = await response.json();
-    return new Response(JSON.stringify(data), {
-      status: 200,
-      headers: { 'Content-Type': 'application/json' },
-    });
-  } catch (error) {
-    return new Response(JSON.stringify({ error: error.message }), {
-      status: 500,
-      headers: { 'Content-Type': 'application/json' },
-    });
-  }
+  console.log(`${url}`);
+  // try {
+  //   const response = await fetch(url);
+  //   if (!response.ok) {
+  //     throw new Error(`Failed to fetch data from Last.fm: ${response.statusText}`);
+  //   }
+  //
+  //   const data = await response.json();
+  //   return new Response(JSON.stringify(data), {
+  //     status: 200,
+  //     headers: { 'Content-Type': 'application/json' },
+  //   });
+  // } catch (error) {
+  //   return new Response(JSON.stringify({ error: error.message }), {
+  //     status: 500,
+  //     headers: { 'Content-Type': 'application/json' },
+  //   });
+  // }
 }
