ec25a34dd79ec82628cef8c95b57aa3f56aa7969
Allan Kent <allan.kent@elerian.ai>
2024-04-07 20:03:41 +0200

    url parsing returns undefined....

--- functions/helloworld.js
+++ functions/helloworld.js
@@ -2,7 +2,7 @@ export async function onRequest(context) {
   const LASTFM_USERNAME = context.env.LASTFM_USERNAME;
   const LASTFM_API_TOKEN = context.env.LAST_FM_API_TOKEN;
   
-  const { urlParams } = new URL(context.request.url);
+  const urlParams = new URL(context.request.url);
   let limit = urlParams.get('limit');
   if (!limit || isNaN(parseInt(limit))) {
     limit = 1; 
